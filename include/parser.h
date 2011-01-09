83e850f03fdcf0b7e721843a66c2f94875559dc2
Alex McGuire <cage433@gmail.com>
2011-01-09 07:44:25 +0000

    Some parser code

--- include/parser.h
+++ include/parser.h
@@ -1,36 +1,53 @@
 /*
-        Expression types
+        expression types
 
         1. Integer
         2. Double
         3. Identifier
-        4. Definition
-        5. Function call
+        4. definition
+        5. function call
         6. Operator call
 */
-typedef struct Function_Call Function_Call;
-typedef struct Function_Definition Function_Definition;
-typedef struct Expression Expression;
-struct Expression {
-        int type;
+enum expression_type {
+        exp_integer,
+        exp_double,
+        exp_identifier,
+        exp_function_call,
+        exp_function_definitiion
+};
+typedef enum expression_type expression_type;
+
+typedef struct function_call function_call;
+typedef struct function_definition function_definition;
+struct expression {
+        expression_type type;
         union {
                 int int_value;
                 double double_value;
                 char *identifier_value;
-                Function_Call *f_call;
-                Function_Definition *f_definition;
+                function_call *f_call;
+                function_definition *f_definition;
         };
 };
 
-struct Function_Call{
-        char *name;
-        int num_exps;
-        Expression* exps;
+typedef struct expression expression;
+
+struct expression_list {
+    expression car;
+    expression_list *cdr;
 };
 
-struct Function_Definition {
+typedef struct expression_list expression_list;
+
+struct function_call{
+        char *name;
+        int num_exps;
+        struct expression* exps;
+};
+
+struct function_definition {
         char *name;
         int num_args;
         char **args;
-        Expression body;
+        expression body;
 };
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,2 +1,54 @@
 #include "parser.h"
+#include "token.h"
+#include <stdio.h>
+#include "string.h"
+#include "stdlib.h"
 
+expression identifier_expression(char *identifier){
+        char *identifier_copy = (char *)calloc(strlen(identifier) + 1, sizeof(char));
+        strcpy(identifier_copy, identifier);
+        expression exp;
+        exp.type = exp_identifier;
+        exp.identifier_value = identifier_copy;
+        return exp;
+}
+
+expression int_expression(int num){
+        expression exp;
+        exp.type = exp_integer;
+        exp.int_value = num;
+        return exp;
+}
+
+expression double_expression(double num){
+        expression exp;
+        exp.type = exp_double;
+        exp.double_value = num;
+        return exp;
+}
+
+expression parse_paren_expression(token_list **tokens){
+        expression exp;
+        return exp;
+}
+
+expression parse_expression(token_list **tokens){
+        token_list *list = *tokens;
+        typed_token first_token = (**tokens).car;
+        switch (first_token.type){
+                case tok_left_paren:
+                        return parse_paren_expression(tokens);
+                case tok_identifier:
+                        (*tokens)++;
+                        return identifier_expression(first_token.identifierValue);
+                case tok_integer:
+                        (*tokens)++;
+                        return int_expression(first_token.intValue);
+                case tok_double:
+                        (*tokens)++;
+                        return double_expression(first_token.doubleValue);
+                default:
+                        printf("Unexpected token\n");
+                        exit(1);
+        }
+}
