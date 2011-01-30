7ae8228a1a642c9f04c36f25d33ff637dec78f50
Alex McGuire <cage433@gmail.com>
2011-01-30 17:53:21 +0000

    Add test to parse double and int

--- test/parser_tests.c
+++ test/parser_tests.c
@@ -36,6 +36,18 @@ START_TEST(test_parse_double)
         fail_unless(expressions_equal(l->car, make_double_expression(10.5)));
 }
 END_TEST
+
+START_TEST(test_parse_double_and_int)
+{
+        char *code = "10.5 19";
+        FILE *s = fmemopen(code, strlen(code), "r");
+        List *l = parse_expressions(s);
+        fail_unless(listlen(l) == 2);
+        fail_unless(expressions_equal(l->car, make_double_expression(10.5)));
+        fail_unless(expressions_equal(l->cdr->car, make_integer_expression(19)));
+}
+END_TEST
+
 Suite *test_parser_suite()
 {
         Suite *s = suite_create ("parser");
@@ -45,6 +57,7 @@ Suite *test_parser_suite()
         tcase_add_test (tc_core, test_parse_int);
         tcase_add_test (tc_core, test_parse_two_ints);
         tcase_add_test (tc_core, test_parse_double);
+        tcase_add_test (tc_core, test_parse_double_and_int);
         suite_add_tcase (s, tc_core);
 
         return s;
