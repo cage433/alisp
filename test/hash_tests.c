cae382e3414a9f2c57af6665b8d9e4409bc73e54
Alex McGuire <cage433@gmail.com>
2011-02-04 20:17:52 +0000

    hash works with adding/removing many keys

--- test/hash_tests.c
+++ test/hash_tests.c
@@ -1,5 +1,7 @@
 #include "check.h"
 #include "hash.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 START_TEST(test_hash_add_remove)
 {
@@ -15,6 +17,27 @@ START_TEST(test_hash_add_remove)
 }
 END_TEST
 
+START_TEST(test_hash_add_lots_of_keys){
+    Hash *hash = hash_create(string_hash_fn, hash_string_equality);
+    int n = 100;
+    char *keys[n];
+    int i;
+    for (i = 0; i < n; ++i){
+        keys[i] = malloc(sizeof(char) * 10);
+        sprintf(keys[i], "%d", i);
+        fail_if(hash_contains(hash, keys[i]));
+        hash_add(hash, keys[i], keys[i]);
+        fail_unless(hash_contains(hash, keys[i]));
+        fail_unless(hash->num_elements == i + 1);
+    }
+    for (i = 0; i < n; ++i){
+        hash_remove(hash, keys[i]);
+        fail_if(hash_contains(hash, keys[i]));
+        fail_unless(hash->num_elements == n - 1 - i);
+    }
+}
+END_TEST
+
 Suite *test_hash_suite()
 {
     Suite *s = suite_create ("hash");
@@ -22,6 +45,7 @@ Suite *test_hash_suite()
     /* Core test case */
     TCase *tc_core = tcase_create ("Core");
     tcase_add_test (tc_core, test_hash_add_remove);
+    tcase_add_test (tc_core, test_hash_add_lots_of_keys);
     suite_add_tcase (s, tc_core);
 
     return s;
