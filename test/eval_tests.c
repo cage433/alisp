d5e6a47d3f7127ed58ad3bb177e4841f3237f40a
Alex McGuire <cage433@gmail.com>
2011-07-26 23:47:12 +0100

    first quote test works

--- test/eval_tests.c
+++ test/eval_tests.c
@@ -356,6 +356,15 @@ START_TEST(test_tagbody_factorial){
 }
 END_TEST
 
+START_TEST(test_quote){
+    List *env = create_env();
+    char *exp = "(quote 4)";
+    boxed_value *v = eval_exp(env, NULL, parse_expression_from_string(exp));
+    boxed_value *v2 = eval_boxed(env, NULL, v);
+    fail_unless(boxed_values_equal(make_boxed_int(4), v2));
+}
+END_TEST
+
 Suite *test_eval_suite ()
 {
     Suite *s = suite_create ("eval");
@@ -388,6 +397,7 @@ Suite *test_eval_suite ()
     tcase_add_test (tc_core, test_tagbody3);
     tcase_add_test (tc_core, test_comparators);
     tcase_add_test (tc_core, test_tagbody_factorial);
+    tcase_add_test (tc_core, test_quote);
     suite_add_tcase (s, tc_core);
 
     return s;
